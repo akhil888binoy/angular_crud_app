af130efbbec1daa9fbd2991616971eecca115488
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2022-11-16 22:35:03 +0530

    routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { EditComponent } from './DataHub/edit/edit.component';
 import { AboutComponent } from './DataHub/about/about.component';
 import { ResumeComponent } from './DataHub/resume/resume.component';
 import { ContactsComponent } from './DataHub/contacts/contacts.component';
-import { DataComponent } from './DataHub/data/data.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,9 +18,8 @@ import { DataComponent } from './DataHub/data/data.component';
     EditComponent,
     AboutComponent,
     ResumeComponent,
-    ContactsComponent,
-    DataComponent
-  ],
+    ContactsComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/data-hub/data-hub-routing.module.ts
+++ src/app/data-hub/data-hub-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from '../DataHub/home/home.component';
+import {AboutComponent} from '../DataHub/about/about.component';
+import { ContactsComponent } from '../DataHub/contacts/contacts.component';
+import { CreateComponent } from '../DataHub/create/create.component';
+import { ResumeComponent } from '../DataHub/resume/resume.component';
 
 const routes: Routes = [
   {
     path: "Datahub/home", component: HomeComponent
+  },
+  {
+    path: "Datahub/about", component: AboutComponent
+  },
+  {
+    path: "Datahub/create", component: CreateComponent
+  },
+  {
+    path: "Datahub/resume", component: ResumeComponent
+  },
+  {
+    path: "Datahub/contacts", component: ContactsComponent
   }
+
 ];
 
 @NgModule({
